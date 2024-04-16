8c0c81a8e3a760f3daea23fde6b4d15130f9dbba
ZoharSimhon <Zoharsimhon13@gmail.com>
2024-04-16 18:32:09 +0300

    added anomaly by distance

--- gnn_clustering.py
+++ gnn_clustering.py
@@ -26,8 +26,6 @@ def clustering_algorithm(graph, embeddings):
     # Perform clustering
     cluster_labels = clusterer.fit_predict(embeddings)
     
-    # print(cluster_labels)
-    # Return cluster labels
     return cluster_labels
 
 #Function to calculte the amount of every cluster
@@ -35,6 +33,26 @@ def count_vectors_in_clusters(labels):
     unique_labels, counts = np.unique(labels, return_counts=True)
     cluster_counts = dict(zip(unique_labels, counts))
     return cluster_counts
+
+# Function to measure distance of a cluster
+def find_centroids(vectors, labels):
+    # Calculate centroids for each cluster
+    centroids = []
+    for cluster in np.unique(labels):
+        cluster_points = vectors[labels == cluster]
+        centroid = np.mean(cluster_points, axis=0)
+        centroids.append(centroid)
+    
+    # Convert centroids to a NumPy array
+    return np.array(centroids)
+
+def calculate_centroid_distances(centroids):
+    distances = []
+    for i in range(len(centroids)):
+        list_without_current = np.delete(centroids, i, axis=0)
+        current_distance = pairwise_distances_argmin_min(centroids, list_without_current, metric='euclidean')[1]
+        distances.append(current_distance[i])
+    return distances
 
 # Function to measure density of a cluster
 def calculate_density(vectors, labels):
@@ -66,7 +84,7 @@ def check_all_anomalies(embeddings, clusters):
     if unusual_clusters:
         print("Number of vectors in each cluster:", cluster_counts)
         print("Unusual clusters (amount):", unusual_clusters)
-    print()
+        print()
         
     
     # Calculate densities in each cluster
@@ -77,6 +95,19 @@ def check_all_anomalies(embeddings, clusters):
     if unusual_clusters:
         print("Densities in each cluster:", cluster_densities)
         print("Unusual clusters (density):", unusual_clusters)
+        print()
+        
+    # Calculate distances between centroids
+    centroids = find_centroids(embeddings, clusters)
+    centroid_distances = calculate_centroid_distances(centroids)
+    
+    # Check for anomaly distances
+    unusual_distances = check_anomalies(centroid_distances)
+    if unusual_distances:
+        print("Distances between centroids:", centroid_distances)
+        print("Unusual distances:", unusual_distances)
+        print()
+        
     print()
     print()
     
